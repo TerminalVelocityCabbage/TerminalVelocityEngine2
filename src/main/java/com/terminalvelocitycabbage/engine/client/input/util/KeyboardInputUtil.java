570916867b1a1b54841245695e6883445f9815b9
Brandon Davis <neusfear@gmail.com>
2024-05-16 18:35:41 -0500

    Fix crash when loss of focus

--- src/main/java/com/terminalvelocitycabbage/engine/client/input/util/KeyboardInputUtil.java
+++ src/main/java/com/terminalvelocitycabbage/engine/client/input/util/KeyboardInputUtil.java
@@ -2,15 +2,18 @@ package com.terminalvelocitycabbage.engine.client.input.util;
 
 import com.terminalvelocitycabbage.engine.client.input.types.ButtonAction;
 
-import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.glfwGetKey;
 
 public class KeyboardInputUtil {
 
     public static ButtonAction getKeyStatus(long window, int glfwKey) {
+        if (window == -1) return null;
         return ButtonAction.fromGLFW(glfwGetKey(window, glfwKey));
     }
 
     public static boolean isKeyPressed(long window, int glfwKey) {
+        if (window == -1) return false;
         return glfwGetKey(window, glfwKey) == GLFW_PRESS;
     }
 
