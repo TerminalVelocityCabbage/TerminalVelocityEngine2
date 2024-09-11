529398c5c91f1f9463815d90008ae7becb310ca6
Brandon Davis <neusfear@gmail.com>
2024-09-10 19:11:54 -0500

    Add docs

--- src/main/java/com/terminalvelocitycabbage/engine/translation/Localizer.java
+++ src/main/java/com/terminalvelocitycabbage/engine/translation/Localizer.java
@@ -15,6 +15,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A class of utilities for localizing game texts. To use the localizer first all language configs shall be registered
+ * with the {@link Language} abbreviation in toml files (en-us.toml for example) on the filesystem. Next all of the
+ * translation keys should be registered with #registerTranslatableText for each of the localizable texts, and finally
+ * the #init method should be called to compile the Localizer. After that any of the registered keys can be localized
+ * to the currently configured language with the various #localize methods in this class.
+ */
 public class Localizer {
 
     Language language;
@@ -31,20 +38,42 @@ public class Localizer {
         this.loadedConfigs = new HashMap<>();
     }
 
+    /**
+     * Localizes the specified translation key to the current language
+     * @param localizableTextKey The key for the localizable text being localized
+     * @return A localized string defined by the provided key
+     */
     public String localize(Identifier localizableTextKey) {
         return translations.get(localizableTextKey);
     }
 
+    /**
+     * Localizes the specified translation key to the current language and substitutes arguments as provided
+     * @param localizableTextKey The key for the localizable text being localized
+     * @param args arguments that the localized text need to have substituted in, in order. {0} will be replaced by the
+     *             string value of the first argument provided {1} the second etc.
+     * @return A localized string defined by the provided key with substituted arguments
+     */
     public String localize(Identifier localizableTextKey, Object... args) {
         return MessageFormat.format(localize(localizableTextKey), args);
     }
 
-    public Identifier registerTranslatableText(String namespace, String path) {
-        Identifier identifier = new Identifier(namespace, path);
+    /**
+     * Registers a translation key to this localizer to be compiled on init
+     * @param namespace The namespace of the translation (usually the entrypoint ID)
+     * @param localizableTextKey The key defined by the localization file for this text being registered
+     * @return The identifier created by this translatable text registry
+     */
+    public Identifier registerTranslatableText(String namespace, String localizableTextKey) {
+        Identifier identifier = new Identifier(namespace, localizableTextKey);
         translations.put(identifier, null);
         return identifier;
     }
 
+    /**
+     * Changes the current language to the specified language
+     * @param newLanguage The language you wish to switch this localizer to
+     */
     public void changeLanguage(Language newLanguage) {
         language = newLanguage;
         init();
@@ -56,6 +85,11 @@ public class Localizer {
         translations = translations1;
     }
 
+    /**
+     * Initializes this localizer by creating a list of config objects from the config files and caching them then
+     * clearing all currently translated texts and going through each of the registered translation keys and gathering
+     * their actual values for the current language or fallback language
+     */
     public void init() {
 
         //Parse and cache configs from localization files if none have been loaded yet
