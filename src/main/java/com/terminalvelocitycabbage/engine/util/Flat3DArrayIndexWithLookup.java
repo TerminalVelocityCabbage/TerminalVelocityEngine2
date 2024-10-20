a625f79eca5afd3a7944e245d23996855145b4a8
Brandon Davis <neusfear@gmail.com>
2024-10-20 11:45:04 -0500

    Add 3d to 1d array utility class

---
+++ src/main/java/com/terminalvelocitycabbage/engine/util/Flat3DArrayIndexWithLookup.java
@@ -0,0 +1,37 @@
+package com.terminalvelocitycabbage.engine.util;
+
+public class Flat3DArrayIndexWithLookup {
+
+    private int arrayDimension;
+    private int[] lookupTable;
+
+    public Flat3DArrayIndexWithLookup(int arrayDimension) {
+        this.arrayDimension = arrayDimension;
+        this.lookupTable = new int[arrayDimension * arrayDimension * arrayDimension * 3];
+
+        // Populate the lookup table
+        for (int x = 0; x < arrayDimension; x++) {
+            for (int y = 0; y < arrayDimension; y++) {
+                for (int z = 0; z < arrayDimension; z++) {
+                    int index = to1DIndex(x, y, z);
+                    lookupTable[index * 3] = x;
+                    lookupTable[index * 3 + 1] = y;
+                    lookupTable[index * 3 + 2] = z;
+                }
+            }
+        }
+    }
+
+    public int to1DIndex(int x, int y, int z) {
+        return x * (arrayDimension * arrayDimension) + y * arrayDimension + z;
+    }
+
+    public int[] to3DCoordinates(int index) {
+        return new int[]{
+                lookupTable[index * 3],
+                lookupTable[index * 3 + 1],
+                lookupTable[index * 3 + 2]
+        };
+    }
+
+}
