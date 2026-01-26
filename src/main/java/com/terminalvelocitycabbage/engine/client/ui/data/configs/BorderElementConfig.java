23300f4a86fde1d7744f5bf748ab8ef26810adb7
Brandon Davis <neusfear@gmail.com>
2026-01-26 17:57:32 -0600

    Remove pointless border builder

--- src/main/java/com/terminalvelocitycabbage/engine/client/ui/data/configs/BorderElementConfig.java
+++ src/main/java/com/terminalvelocitycabbage/engine/client/ui/data/configs/BorderElementConfig.java
@@ -9,27 +9,4 @@ public record BorderElementConfig(Color color, BorderWidth width) {
         if (color == null) color = new Color(0, 0, 0, 255);
         if (width == null) width = new BorderWidth();
     }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private Color color;
-        private BorderWidth width;
-
-        public Builder color(Color color) {
-            this.color = color;
-            return this;
-        }
-
-        public Builder width(BorderWidth width) {
-            this.width = width;
-            return this;
-        }
-
-        public BorderElementConfig build() {
-            return new BorderElementConfig(color, width);
-        }
-    }
 }
