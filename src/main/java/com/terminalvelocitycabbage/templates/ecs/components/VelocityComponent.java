706e36e817f04df8827c6215fff0c1b03965e511
Brandon Davis <neusfear@gmail.com>
2026-03-07 13:55:28 -0600

    add parsers for position and velocity

--- src/main/java/com/terminalvelocitycabbage/templates/ecs/components/VelocityComponent.java
+++ src/main/java/com/terminalvelocitycabbage/templates/ecs/components/VelocityComponent.java
@@ -8,6 +8,20 @@ public class VelocityComponent implements Component {
     Vector3f velocity;
 
     @Override
+    public void parseComponentField(String field, String value) {
+        String cleanedValue = value.replace("[", "").replace("]", "");
+        String[] split = cleanedValue.split(",");
+        for (int i = 0; i < split.length; i++) split[i] = split[i].trim();
+        switch (field) {
+            case "velocity" -> {
+                if (split.length == 3) {
+                    setVelocity(Float.parseFloat(split[0]), Float.parseFloat(split[1]), Float.parseFloat(split[2]));
+                }
+            }
+        }
+    }
+
+    @Override
     public void setDefaults() {
         velocity = new Vector3f(0, 0, 0);
     }
